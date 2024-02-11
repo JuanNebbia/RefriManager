d64e7d853a0d92b9b72c62f8645e683f81eaa5f4
Juan Nebbia <juan.nebbia@gmail.com>
2024-02-11 01:45:59 -0300

    backend: Delete and add buckets on refrigerators creation/deletion

--- backend/src/controllers/refrigerators.controller.js
+++ backend/src/controllers/refrigerators.controller.js
@@ -1,7 +1,9 @@
+import BucketsService from "../services/buckets.service.js";
 import RefrigeratorsService from "../services/refrigerators.service.js";
 import { HTTP_CREATED, HTTP_SUCCESS } from "../utils/constants.util.js";
 
 const refrigeratorsService = new RefrigeratorsService();
+const bucketsService = new BucketsService();
 
 class RefrigeratorsController {
   static getAllRefrigerators = async (req, res, next) => {
@@ -26,7 +28,20 @@ class RefrigeratorsController {
   static addRefrigerator = async (req, res, next) => {
     const payload = req.body;
     try {
-      const refrigerators = await refrigeratorsService.addOne(payload);
+      const newRefrigerator = await refrigeratorsService.addOne(payload);
+      for (let i = 0; i < payload.total_capacity; i++) {
+        const newBucket = {
+          refrigerator_id: newRefrigerator._id,
+          flavor_id: null,
+          position:
+            i + 1 <= newRefrigerator.total_capacity / 2
+              ? i + 1
+              : i + 1 - newRefrigerator.total_capacity / 2,
+          side: i + 1 <= newRefrigerator.total_capacity / 2 ? 0 : 1,
+        };
+        await bucketsService.addOne(newBucket);
+      }
+      const refrigerators = await refrigeratorsService.getAll();
       res.status(HTTP_CREATED).send(refrigerators);
     } catch (error) {
       next(error);
@@ -47,8 +62,17 @@ class RefrigeratorsController {
   static deleteRefrigerator = async (req, res, next) => {
     const { rid } = req.params;
     try {
-      const refrigerators = await refrigeratorsService.deleteOne(rid);
-      res.status(HTTP_SUCCESS).send(refrigerators);
+      await refrigeratorsService.deleteOne(rid);
+      const buckets = await bucketsService.getAll();
+      const refriBuckets = buckets.filter(
+        (bucket) =>
+          JSON.stringify(bucket.refrigerator_id) === JSON.stringify(rid)
+      );
+      refriBuckets.forEach(async (bucket) => {
+        await bucketsService.deleteOne(bucket._id);
+      });
+      const refrigerators = await refrigeratorsService.getAll();
+      res.status(HTTP_CREATED).send(refrigerators);
     } catch (error) {
       next(error);
     }
